9eff4899f79480ebf756f072ba80bf480cc13fdc
alloneinfo <kannychak@qq.com>
2026-05-27 11:20:56 +0800

    ci(workflow): 更新x86-64构建 workflow 的luci默认版本
    将luci默认版本从25.12.0-rc2改为正式版25.12.0，并新增25.12.0作为可选版本

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -62,10 +62,15 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+
 PACKAGES="$PACKAGES luci-app-homeproxy"
-PACKAGES="$PACKAGES luci-app-zerotier"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+
 PACKAGES="$PACKAGES luci-proto-wireguard"
 PACKAGES="$PACKAGES luci-app-vlmcsd"
+
+PACKAGES="$PACKAGES luci-app-zerotier"
+PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 #25.12
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
